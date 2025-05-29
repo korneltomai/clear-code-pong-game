064037f2b19531a60010e164195c3a2bf91dc7c5
T3tr1s <tomai.kornelmunka@gmail.com>
2025-05-29 23:42:22 +0200

    Add: "Pong"

--- code/main.py
+++ code/main.py
@@ -1,1 +1,42 @@
 from settings import * 
+from sprites import *
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Pong")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+        self.paddle_sprites = pygame.sprite.Group()
+
+        # sprites
+        self.player = Player((self.all_sprites, self.paddle_sprites), COLORS["paddle"], POS["player"], SPEED["player"])
+        self.ball = Ball(self.all_sprites, self.paddle_sprites)
+
+    def run(self):
+
+        while self.running:
+            delta_time = self.clock.tick(60) /  1000
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # update
+            self.all_sprites.update(delta_time)
+
+            # draw
+            self.display_surface.fill(COLORS["bg"])
+            self.all_sprites.draw(self.display_surface)
+
+            pygame.display.update()
+
+        pygame.quit()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
